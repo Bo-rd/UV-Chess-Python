a61d6b8b0fe6ee40b1f7540e5a262d608f0c4572
Max <maxdiamond117@gmail.com>
2022-05-18 19:40:58 -0600

    Changed name to UV chess

--- screen.py
+++ screen.py
@@ -2,7 +2,7 @@ import pygame
 background_colour = (0,0,0)
 (width, height) = (300, 200)
 screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption('Tutorial 1')
+pygame.display.set_caption('UV Chess')
 screen.fill(background_colour)
 pygame.display.flip()
 running = True
