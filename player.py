d89ae37459534eb350557cf5453a2423beeca7f7
andrew-jp <74275670+andrew-jp@users.noreply.github.com>
2022-05-23 12:22:09 -0600

    added basic player class

---
+++ player.py
@@ -0,0 +1,33 @@
+from matplotlib.pyplot import pie
+import pygame
+import piece
+import re
+
+class Player():
+
+    def __init__(self, playerId, team, pieceList):
+        self.id = playerId
+        self.team = team
+        self.pieces = pieceList
+        
+        for piece in pieceList: # get reference to players king
+            if piece.getPieceId().endswith('king'):
+                self.king = piece
+
+        # if checked, valid moves are restricted to unchecking
+        # a function should verify status at beginning of every
+        # turn
+        self.inCheck = False 
+
+    def __str__(self):
+        return f"ID: {self.id} Team: {self.team}"
+
+    def getPlayerId(self):
+        return self.id
+
+    def getPieces(self):
+        return self.pieces
+
+    def kingStatus(self):
+        if self.king.checked:
+            self.inCheck = True
