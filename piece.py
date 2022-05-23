27493a33ec8b7aafaf7ea439e3bfab0bd77f5707
Joshua Kitchen <57072030+kitchej@users.noreply.github.com>
2022-05-22 20:45:23 -0600

    created piece abstract class

--- piece.py
+++ piece.py
@@ -10,20 +10,22 @@ class Piece(abc.ABC):
         self.y = yPos
         self.size = 25
         self.team = team
-        self.graphic = graphic
+        self.graphic = graphic  # picture of the piece
         self.captured = False
         self.pieceId = pieceId
         self.tilesList = tilesList
         self.validTiles = []  # contains a list of valid tiles to move to. Set by showLegalMoves
 
+    def __str__(self):
+        return f"{self.pieceId} at x: {self.x}, y: {self.y}"
+
     def draw(self, surface):
         pygame.draw.rect(surface, (0, 0, 255), pygame.Rect(
                 self.x*self.size, self.y*self.size, self.size, self.size))
 
-
     @abc.abstractmethod
     def showLegalMoves(self):
-        '''highlights all tiles that are legal moves'''
+        '''highlights all tiles that are legal moves and adds them to self.validTiles'''
         pass
 
     def setPos(self, x, y):
@@ -31,6 +33,9 @@ class Piece(abc.ABC):
         if x > 15 or y > 15:
             return False
         if x < 0 or y < 0:
+            return False
+        tile = self.tilesList[y][x]
+        if tile not in self.validTiles:
             return False
         self.x = x
         self.y = y
